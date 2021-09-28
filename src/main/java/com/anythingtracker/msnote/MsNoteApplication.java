680f6ede3daf2815c71bf296078962c0d5874e50
Valeriia Starosek <valeriiastarosek@gmail.com>
2021-09-28 11:00:48 -0700

    initial commit Closes TRKR-2

---
+++ src/main/java/com/anythingtracker/msnote/MsNoteApplication.java
@@ -0,0 +1,13 @@
+package com.anythingtracker.msnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNoteApplication.class, args);
+	}
+
+}
